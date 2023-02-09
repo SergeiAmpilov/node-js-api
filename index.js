625bdc0bb29db02fc50b08745435c3e9a01af70f
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-09 19:17:54 +0300

    feat: middleware and error processing

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ import { router as userRouter } from './users/users.js';
 const port  = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Время поступления запроса: ', new Date().toString());
+  next();
+});
+
 app.get('/hello', (req, res) => {
+  throw new Error('Error!!!');
   res.send('Hello worls');
 })
 
 app.use('/users', userRouter);
+
+// обработчик ошибки должен добавляться после всех обработчиков use
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).end(err.message);
+});
 
 app.listen(port, () => {
   console.log(`Server has been started on http://localhost:${port}`);
--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log('check users token');
+  next();
+})
+
 router.post('/login', (req, res) => {
   res.send('login')
 });
