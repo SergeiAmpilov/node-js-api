82c781a2ae7ad718f76d8d00b94cd951c8d38654
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-09 16:34:27 +0300

    basic routing

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ import express from 'express';
 const port  = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
+app.all('/hello', (req, res, next) => {
+  console.log('All');
+  next();
+});
+
+const cBack1 = (req, res, next) => {
+  console.log('callback', 1);
+  next();
+};
+
+const cBack2 = (req, res, next) => {
+  console.log('callback', 2);
+  next();
+};
+
+
+app.get('/he(la)?llo', [cBack1, cBack2], (req, res) => {
   res.send('Hello, world');
 });
+
+app.get(/.*a$/, (req, res) => {
+  res.send('Hello regexp');
+});
+
+app.route('/user')
+  .all((req, res, next) => {
+    console.log('/user');
+    next();
+  })
+  .get((req, res) => res.send('Get /user') )
+  .post((req, res) => res.send('Post /user') )
 
 app.listen(port, () => {
   console.log(`Server has been started on http://localhost:${port}`);
