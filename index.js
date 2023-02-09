268773b5f1cc7632148d8502805289af5092a9cb
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-09 17:34:56 +0300

    feat: response variations

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const cBack2 = (req, res, next) => {
 
 
 app.get('/he(la)?llo', [cBack1, cBack2], (req, res) => {
+  res.cookie('token', 'Sergei', {
+    domain: '*',
+    path: '/hello',
+    secure: true,
+    maxAge: 60000
+  });
   res.send('Hello, world');
 });
 
@@ -29,11 +35,29 @@ app.get(/.*a$/, (req, res) => {
 
 app.route('/user')
   .all((req, res, next) => {
+    res.set('Content-Type', 'text/plain');
+    res.type('application/json');
+    res.append('Worning', 'code');
     console.log('/user');
     next();
   })
   .get((req, res) => res.send('Get /user') )
   .post((req, res) => res.send('Post /user') )
+
+app.get('/rq', (req, res) => {
+  // res.status(201).send({ success: true });
+  res.status(202).json({ success: true });
+});
+
+app.get('/down', (req, res) => {
+  res.download('./test.txt', 'tessssst.txt');
+});
+
+app.get('/redirect', (req, res) => {  
+  res.redirect(301, '/user');
+})
+
+
 
 app.listen(port, () => {
   console.log(`Server has been started on http://localhost:${port}`);
