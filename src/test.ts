4e4cfd2574018aa38b774c1aeb2ff6ebd9296898
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-02 11:33:50 +0300

    decoratod for props, methods and params

--- src/test.ts
+++ src/test.ts
@@ -15,16 +15,52 @@ function Logger() {
   }
 }
 
+function Method(target: Object, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
+
+  propertyDescriptor.value = function (...args: any[]) {   
+    return typeof args[0] === 'number'  
+            ? args[0] * 10
+            : args[0];
+  }
+
+}
+
+function Prop(target: Object, propertyKey: string) {
+
+  let value: number;
+
+  const getter = () => {
+    console.log('Get!');
+    return value;
+  }
+
+  const setter = (newValue: number) => {
+    console.log('Set!!');
+    value = newValue;
+  }
+
+  Object.defineProperty(target, propertyKey, {
+    get: getter,
+    set: setter
+  });
+
+}
+
+function Param(target: Object, propertyKey: string, index: number) {
+  console.log(propertyKey, index);
+}
 
 @Logger()
 @Component(15)
 export class User {
-    id: Number;
+    @Prop id: Number;
     
-    updateId(newId: number) {
+    @Method
+    updateId(@Param newId: number) {
       this.id = newId;
       return this.id;      
     }
 }
 
 console.log(new User().id);
+console.log(new User().updateId(2));
