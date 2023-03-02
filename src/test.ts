ddc4c22a890a98975236cd35bbb5e4e8edbeb3ad
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-02 12:44:47 +0300

    feat: library reflect-metadata

--- src/test.ts
+++ src/test.ts
@@ -1,66 +1,36 @@
-function Component(id: number) {
+import 'reflect-metadata';
+
+function Injectable(key: string) {
+  return (target: Function) => {
+    Reflect.defineMetadata(key, 1, target);
+    const meta = Reflect.getMetadata(key, target);
+
+    console.log(meta);
+  }
   
-  console.log('init component');
-  return (target: Function) => {
-    console.log('run component');
-    target.prototype.id = id;
-  }
-}
-
-
-function Logger() {
-  console.log('init logger');
-  return (target: Function) => {
-    console.log('run logger');
-  }
-}
-
-function Method(target: Object, propertyKey: string, propertyDescriptor: PropertyDescriptor) {
-
-  propertyDescriptor.value = function (...args: any[]) {   
-    return typeof args[0] === 'number'  
-            ? args[0] * 10
-            : args[0];
-  }
 
 }
 
-function Prop(target: Object, propertyKey: string) {
+function Prop(target: Object, name: string) {
 
-  let value: number;
+}
 
-  const getter = () => {
-    console.log('Get!');
-    return value;
+function Inject() {
+  
+}
+
+
+@Injectable('C')
+export class C {
+
+  @Prop prop: number;
+}
+
+@Injectable('D')
+export class D {
+  constructor(@Inject('C') c: C) {
+
   }
 
-  const setter = (newValue: number) => {
-    console.log('Set!!');
-    value = newValue;
-  }
-
-  Object.defineProperty(target, propertyKey, {
-    get: getter,
-    set: setter
-  });
-
+  @Prop prop: number;
 }
-
-function Param(target: Object, propertyKey: string, index: number) {
-  console.log(propertyKey, index);
-}
-
-@Logger()
-@Component(15)
-export class User {
-    @Prop id: Number;
-    
-    @Method
-    updateId(@Param newId: number) {
-      this.id = newId;
-      return this.id;      
-    }
-}
-
-console.log(new User().id);
-console.log(new User().updateId(2));
